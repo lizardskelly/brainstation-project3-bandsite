5429468a61782d48a579988924c2b8e5293294d1
Liz Meeker <lizmeeker90@gmail.com>
2022-05-19 23:37:59 -0400

    added get and post functions for comments

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,27 +1,19 @@
-// building the comment array with its placeholder posts
-// note: im setting the avatar to my placeholder asset
-// because i'm assuming with the next sprint the api we use
-// will include differing user avatars
-let commentArray = [
-  {
-    username: "Miles Acosta",
-    timestamp: "12/20/2020",
-    avatar: "./assets/images/avatar-placeholder.jpg",
-    commentBody: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough."
-  },
-  {
-    username: "Emilie Beach",
-    timestamp: "01/09/2021",
-    avatar: "./assets/images/avatar-placeholder.jpg",
-    commentBody: "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day."
-  },
-  {
-    username: "Conner Walton",
-    timestamp: "02/17/2021",
-    avatar: "./assets/images/avatar-placeholder.jpg",
-    commentBody: "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains."
-  }
-];
+let authKey
+// retrieves and sets authentication key
+const retrieveAuthKey = () => {
+  return axios.get('https://project-1-api.herokuapp.com/register')
+    .then(response => {
+      authKey = response.data.api_key;
+    })
+}
+
+// retrieving comment data from API
+const getComments = () => {
+  return axios.get('https://project-1-api.herokuapp.com/comments?api_key=' + authKey)
+    .then(response => {
+      renderComments(response.data);
+    })
+}
 
 // clears all comments on the page so they can be
 // redisplayed with the updated comment array
@@ -32,18 +24,18 @@ const clearComments = postParent => {
 }
 
 // function takes values from objects in comment array
-// and displays them in the comments section
+// and builds them to be rendered later
 const displayComment = (postParent, comment) => {
   const post = document.createElement('div');
   post.classList.add("post");
   postParent.prepend(post);
-
+  
   // creates the various elements of each post,
   // sets them to the proper values pulled from the array,
   // and appends them to the dom in the proper locations
   let userAvatar = document.createElement('img');
   userAvatar.classList.add('post__avatar');
-  userAvatar.setAttribute('src', comment.avatar);
+  userAvatar.setAttribute('src', '../assets/images/avatar-placeholder.jpg');
   post.appendChild(userAvatar);
 
   let textWrapper = document.createElement('div');
@@ -56,21 +48,29 @@ const displayComment = (postParent, comment) => {
 
   let name = document.createElement('p');
   name.classList.add('post__username');
-  name.innerHTML = comment.username;
+  name.innerHTML = comment.name;
   infoWrapper.appendChild(name);
 
   let time = document.createElement('p');
   time.classList.add('post__timestamp');
-  time.innerHTML = comment.timestamp;
+  time.innerHTML = formattedDate(comment.timestamp);
   infoWrapper.appendChild(time);
 
   let text = document.createElement('p');
   text.classList.add('post__text');
-  text.innerHTML = comment.commentBody;
+  text.innerHTML = comment.comment;
   textWrapper.appendChild(text);
 }
 
-const displayComments = comments => {
+//formats the date timestamp to something readable
+const formattedDate = timestamp => {
+  const timeNum = Number(timestamp);
+  const date = new Date(timeNum);
+  return (date.getMonth() + 1) + '/' + date.getDate() + '/' + date.getFullYear();
+}
+
+//renders the comments on the page
+const renderComments = comments => {
   const postParent = document.getElementById("postParent");
 
   clearComments(postParent);
@@ -79,7 +79,6 @@ const displayComments = comments => {
     displayComment(postParent, comments[i]);
   }
 }
-displayComments(commentArray);
 
 // form submission event listener 
 const commentForm = document.querySelector('.comments__form');
@@ -90,39 +89,39 @@ commentForm.addEventListener('submit', submitComment = event => {
   document.getElementById('username').classList.remove('comments__input--error');
   document.getElementById('comment-body').classList.remove('comments__input--error');
 
-  let newPost = {};
+  let newComment = {};
 
   // checks username input for errors and sets value if good
   const username = event.target.username.value;
   if(username.length > 0) {
-    newPost.username = username;
+    newComment.name = username;
   } else {
     document.getElementById('username').classList.add('comments__input--error');
     return;
   }
-  
-  // formats date and sets it to the correct value
-  let now = new Date()
-  newPost.timestamp = (now.getMonth() + 1) + '/' + now.getDate() + '/' + now.getFullYear();
-  // sets the avatar to the src of the avatar on the page
-  // i'm assuming we'll define the src as an image from
-  // the api in the next sprint
-  let userAvatar = document.querySelector('.comments__form-avatar');
-  newPost.avatar = userAvatar.src
 
   //checks comment body input for errors and sets value if good
   const commentBody = event.target.commentBody.value;
   if (commentBody.length > 0) {
-    newPost.commentBody = commentBody;
+    newComment.comment = commentBody;
   } else {
     document.getElementById('comment-body').classList.add('comments__input--error');
     return;
   }
 
-  // adds the new comment object the comment array
-  commentArray.push(newPost);
+  // posts the new comment to the api
+  postComment(newComment);
 
   // resets the form and displays the new comment
   commentForm.reset();
-  displayComments(commentArray);
 });
+
+const postComment = (comment) => {
+  return axios.post(
+    'https://project-1-api.herokuapp.com/comments?api_key=' + authKey,
+    comment,
+    {headers: {'content-type': 'application/json'}}
+  ).then(getComments)
+}
+
+retrieveAuthKey().then(getComments);
